ed000c9f5a4d7f8eef035c0dd7b679302d8e28a4
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2021-07-06 22:56:11 -0300

    update

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
 module.exports = {
-  "type": process.env.DB_TYPE,
-  "host": process.env.DB_HOST, // || "localhost",
-  "port": process.env.DB_PORT, // || 5432,
-  "username": process.env.DB_USERNAME, // || "postgres",
-  "password": process.env.DB_PASSWORD, // || "postgres",
-  "database": process.env.DATABASE,// || "postgres",
+  "type": process.env.DB_TYPE || "postgres",
+  "url":process.env.DB_URI,
+  // "host": process.env.DB_HOST, // || "localhost",
+  // "port": process.env.DB_PORT, // || 5432,
+  // "username": process.env.DB_USERNAME, // || "postgres",
+  // "password": process.env.DB_PASSWORD, // || "postgres",
+  // "database": process.env.DATABASE,// || "postgres",
   "synchronize": false,
   "logging": false,
   "extra": {
